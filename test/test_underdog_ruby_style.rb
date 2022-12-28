0f8dd80e87bc06c9685fcb4cfad0b131f11396a4
Trevor John <trevor@john.tj>
2022-12-28 12:12:07 -0300

    Upgrade rubocop for ruby 3.2 support
    - delete test. Failing because there is a contradiction in how we setup
      this gem. It sets rails styling but doesn't load rails in the test
      environment and therefore `assert_not_nil` is not defined

--- test/test_underdog_ruby_style.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class TestUnderdogRubyStyle < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::UnderdogRubyStyle::VERSION
-  end
-end
